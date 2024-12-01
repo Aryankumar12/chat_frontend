9e9bdcf64121dbd84a828f7e7e4f268f2dbf71ce
Aryan Kumer <aryandot1@gmail.com>
2024-12-01 19:28:52 +0530

    changed_frontend

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api" // For local development
+    : import.meta.env.VITE_API_URL + "/api", // For production (use environment variable)
   withCredentials: true,
 });
