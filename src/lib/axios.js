cb83a52ce5d4eb1d7e05723bb8669b5c38972a5c
Aryan Kumer <aryandot1@gmail.com>
2024-12-01 19:52:40 +0530

    changed code

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development"
     ? "http://localhost:5001/api" // For local development
-    : import.meta.env.VITE_API_URL + "/api", // For production (use environment variable)
+    : "https://chat-backend-wuui.onrender.com" + "/api", // For production (use environment variable)
   withCredentials: true,
 });
